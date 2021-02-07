dd2e0df524b76ad6e3e9c65218c59b6f7a26c957
Julio Augusto <julioams@icloud.com>
2021-02-07 15:35:47 -0300

    feat: create first content in home screen

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 // Components
-import { Container } from '../../components'
+import { Container, Content, Title } from '../../components'
 
 // Styles
 import * as S from './styles'
@@ -13,6 +13,9 @@ const Home = () => {
   return (
     <Container>
       <S.Logo source={logoImg} resizeMode="contain" />
+      <Content>
+        <Title size={18} weight="bold" color="gray2">Desenvolvimento</Title>
+      </Content>
     </Container>
   )
 }
