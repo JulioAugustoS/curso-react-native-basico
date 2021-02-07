e8ad683003cd418ac6b73df812f0448d8f84875d
Julio Augusto <julioams@icloud.com>
2021-02-07 13:48:46 -0300

    feat: import and export itemlist component

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import Title from './Title'
 import Course from './Course'
 import Tag from './Tag'
 import Card from './Card'
+import ItemList from './ItemList'
 
 export {
   Title,
   Course,
   Tag,
-  Card
+  Card,
+  ItemList
 }
