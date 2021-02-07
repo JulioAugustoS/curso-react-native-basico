a31a3aa55cc4106c398f7647c0d43ef4eafa2919
Julio Augusto <julioams@icloud.com>
2021-02-07 16:22:54 -0300

    feat: create styles components for course component

---
+++ src/components/Course/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+  width: 130px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+`
+
+export const Content = styled.View`
+  margin-top: 12px;
+`
+
+export const Infos = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 5px;
+`
