ab74e6b113bf7900ee25ce06eecc305f21e16863
Julio Augusto <julioams@icloud.com>
2021-02-07 12:02:23 -0300

    feat: create course component

---
+++ src/components/Course/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View } from 'react-native'
+
+// Components
+import { Title } from '../index'
+
+const Course = () => {
+  return (
+    <View>
+      <Title>React</Title>
+      <View>
+        <Title>12 hrs</Title>
+      </View>
+    </View>
+  )
+}
+
+export default Course
