00113cd8a69fd5905a667c5f3d02b0c81ff5cc5b
Julio Augusto <julioams@icloud.com>
2021-02-07 15:10:48 -0300

    feat: add themeprovider in index.js for styled component theme

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { ThemeProvider } from 'styled-components'
 
+// Theme
+import light from './theme/light'
+
+// Screens
 import Home from './screens/Home'
 
 const App = () => {
-  return <Home />
+  return (
+    <ThemeProvider theme={light}>
+      <Home />
+    </ThemeProvider>
+  )
 }
 
 export default App
