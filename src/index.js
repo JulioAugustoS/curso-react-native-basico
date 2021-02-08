15895d0d4c54b4304381cd7e24da6a8100a7a0f5
Julio Augusto <julioams@icloud.com>
2021-02-07 21:09:02 -0300

    feat: finish create layout two pages

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import light from './theme/light'
 
 // Screens
 import Home from './screens/Home'
+import CourseDetails from './screens/CourseDetails'
 
 const App = () => {
   return (
     <ThemeProvider theme={light}>
-      <Home />
+      {/* <Home /> */}
+      <CourseDetails />
     </ThemeProvider>
   )
 }
