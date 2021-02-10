ad221a91a9793aef4cf4d7aa213698bae170f2fd
Julio Augusto <julioams@icloud.com>
2021-02-10 13:11:57 -0300

    feat: add comment

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -6,7 +6,11 @@ import * as S from './styles'
 const Card = ({ uri }) => {
   return (
     <S.Container>
-      <S.Img source={uri} resizeMode="contain" />
+      {/*
+        Alterar o formato de recebimento no source, pois como vai vir via url a imagem
+        o parametro deve ser passado dentro de uma uri
+      */}
+      <S.Img source={{ uri: uri }} resizeMode="contain" />
     </S.Container>
   )
 }
