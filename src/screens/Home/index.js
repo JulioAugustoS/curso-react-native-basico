bd04d8a499c2a7507c5a3314fc97a9e3bf78b986
Julio Augusto <julioams@icloud.com>
2021-02-10 11:40:11 -0300

    feat: created new steps
    create useState for courses
    create useEffect for setState with courses data
    add console.log for show state with courses list

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View } from 'react-native'
+import list from './courses.json'
 
 // Components
 import { Container, Content, Title, Course } from '../../components'
@@ -20,6 +21,25 @@ import googleAdsImg from '../../assets/images/google-ads.png'
 import googleAnalyticsImg from '../../assets/images/google-analytics.png'
 
 const Home = () => {
+  /**
+   * Criar o estado com o valor incial sendo um Array vazio
+   */
+  const [coursesList, setCoursesList] = useState([])
+
+  /**
+   * Executar o useEffect quando o componente for montado atualizando o estado com os dados dos cursos
+   */
+  useEffect(() => {
+    setTimeout(() => {
+      setCoursesList(list.categories);
+    }, 1000);
+  }, []);
+
+  /**
+   * Adicionar um console.log para mostrar o estado atualizado
+   */
+  console.log("Lista de Cursos: ", coursesList)
+
   return (
     <Container>
       <S.Logo source={logoImg} resizeMode="contain" />
