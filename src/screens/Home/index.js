6a5ad44a1f3134643c606ee758cea01f2c641063
Julio Augusto <julioams@icloud.com>
2021-02-10 21:45:31 -0300

    feat: add navigation in home

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react'
 import { View } from 'react-native'
 import list from './courses.json'
 
+// Api
+import api from '../../config/api'
+
 // Context
 import { useCourses } from '../../context/courses'
 
@@ -14,53 +17,43 @@ import * as S from './styles'
 // Images
 import logoImg from '../../assets/images/logo.png'
 
-const Home = () => {
-  /** Desestruture o useCourses() para ter acesso ao estado e a função de atualizar
-   * o estado no contexto
-   */
+const Home = ({ navigation }) => {
   const { coursesList, setCoursesList } = useCourses()
 
-  /**
-   * Criar o estado com o valor incial sendo um Array vazio
-   */
-  // const [coursesList, setCoursesList] = useState([])
+  const findCourses = async () => {
+    try {
+      const res = await api.get('/categories?_embed=courses')
 
-  /**
-   * Executar o useEffect quando o componente for montado atualizando o estado com os dados dos cursos
-   */
+      console.log("RETORNO DA CONSULTA: ", res)
+      setCoursesList(res.data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   useEffect(() => {
-    setTimeout(() => {
-      setCoursesList(list.categories);
-    }, 1000);
+    findCourses()
   }, []);
 
-  /**
-   * Adicionar um console.log para mostrar o estado atualizado
-   */
   console.log("Lista de Cursos: ", coursesList)
 
   return (
     <Container>
       <S.Logo source={logoImg} resizeMode="contain" />
-
-      {/* Mapear o estado de cursos para renderizar as categorias na tela */}
       {coursesList.map((item, index) => (
-        /** Adicionar o index como unic key no componente View */
         <View key={index}>
           <Content>
-            {/* Substituir o nome do curso pela variavel referente ao nome retornado pelo map */}
             <Title size={18} weight="bold" color="gray2">{item.category}</Title>
           </Content>
           <S.Swipper>
-            {/* Mapear o array courses dentro do item para listar os cursos */}
             {item.courses.map((course) => (
-              /** Substituir os dados do curso pelos dados retornado no map */
               <Course
                 key={course.id}
                 image={course.logo}
                 title={course.name}
                 time={course.duration}
                 nivel={course.level}
+                onPress={() => navigation.navigate('CourseDetail', { id: course.id })}
               />
             ))}
           </S.Swipper>
