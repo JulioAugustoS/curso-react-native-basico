7fbc49c20b8559b226910967b71d06a8b7e888dc
Julio Augusto <julioams@icloud.com>
2021-02-10 23:07:49 -0300

    feat: integrate course details with api

--- src/screens/CourseDetails/index.js
+++ src/screens/CourseDetails/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { TouchableOpacity } from 'react-native'
+import { Alert } from 'react-native'
 import { useRoute } from '@react-navigation/native'
 import YoutubePlayer from "react-native-youtube-iframe";
 
@@ -15,41 +15,80 @@ import {
   ItemList,
   TextArea,
   Button,
-  Comment
+  Comment,
+  Loading
 } from '../../components'
 
 // Styles
 import * as S from './styles'
 
 // Images
-import starImg from '../../assets/icons/star.png'
 import checkImg from '../../assets/icons/check.png'
+import checkedImg from '../../assets/icons/checked.png'
 import circleImg from '../../assets/icons/circle.png'
 import chevronLeftImg from '../../assets/icons/chevron-left.png'
 
 const CourseDetails = ({ navigation }) => {
   const { params } = useRoute()
   const [course, setCourse] = useState(null)
+  const [moduleSelect, setModuleSelect] = useState(null)
+  const [comment, setComment] = useState('')
+  const [loading, setLoading] = useState(true)
 
+  /**
+   * 01 - Crie essa função primeiro para trazer os dados do curso pelo ID
+   */
   const findCourse = async () => {
     try {
       const res = await api.get(`/courses/${params.id}?_embed=comments`)
 
-      console.log("RETORNO DO CURSO: ", res)
       setCourse(res.data)
+      setModuleSelect(res.data?.content[0])
+      setLoading(false)
     } catch (err) {
-      console.log(err)
+      console.log("Error find course: ", err)
+      setLoading(false)
     }
   }
 
+  /**
+   * 02 - Execute a função com um timeout de 1s para simular uma requisição a um servidor
+   */
   useEffect(() => {
-    findCourse()
+    setTimeout(() => {
+      findCourse()
+    }, 1000)
   }, [])
 
-  console.log("CURSO: ", course)
+  /**
+   * 03 - Crie essa função para enviar os dados do campo de comentario para a api
+   */
+  const handleSendComment = async () => {
+    if (!comment) {
+      return Alert.alert('Preencha o campo de comentário para continuar!')
+    }
+
+    setLoading(true)
+
+    try {
+      await api.post('/comments', {
+        body: comment,
+        courseId: params.id
+      })
+
+      findCourse()
+      setComment('')
+      setLoading(false)
+      Alert.alert('Tudo Certo!', 'Seu comentário foi adicionado com sucesso!')
+    } catch (err) {
+      console.log("Error Send Comment: ", err.response)
+      setLoading(false)
+    }
+  }
 
   return (
     <Container>
+      {loading && <Loading />}
       <S.ContentPlayer>
         <S.BackButton onPress={() => navigation.goBack()}>
           <Icon uri={chevronLeftImg} />
@@ -57,19 +96,17 @@ const CourseDetails = ({ navigation }) => {
         <YoutubePlayer
           height={250}
           play={false}
-          videoId={"C8M94QLJy0o"}
+          // 06 - Adicione o valor do video que está armazenado no estado de module select
+          videoId={moduleSelect?.video}
         />
       </S.ContentPlayer>
       <Content>
         <S.InfosBase>
           <S.Infos>
-            <Title size={18} weight="bold" color="green">{course?.content[0].title}</Title>
+            <Title size={18} weight="bold" color="green">{moduleSelect?.title}</Title>
             <Title size={16} color="gray2">{course?.teacher}</Title>
             <Title color="gray2">{`${course?.duration} hrs`}</Title>
           </S.Infos>
-          <TouchableOpacity onPress={() => {}}>
-            <Icon uri={starImg} />
-          </TouchableOpacity>
         </S.InfosBase>
 
         <S.Section>
@@ -83,7 +120,14 @@ const CourseDetails = ({ navigation }) => {
           <Title size={18} weight={600} color="gray2">Conteudo</Title>
           <S.List>
             {course?.content.map((module, index) => (
-              <ItemList key={index} icon={checkImg} text={module?.title} />
+              <ItemList
+                key={index}
+                // 04 - Faça uma verificação para exibir o icone de acordo com o item selecionado
+                icon={moduleSelect?.title === module?.title ? checkedImg : checkImg}
+                text={module?.title}
+                // 05 - Adicione uma ação para salvar no estado o modulo selecionado
+                onPress={() => setModuleSelect(module)}
+              />
             ))}
           </S.List>
         </S.Section>
@@ -93,10 +137,12 @@ const CourseDetails = ({ navigation }) => {
           <TextArea
             multiline={true}
             numberOfLines={5}
+            onChangeText={(e) => setComment(e)}
+            value={comment}
             placeholder="Deixe seu comentário"
           />
           <S.ButtonContent>
-            <Button title="Publicar" onPress={() => {}} />
+            <Button title="Publicar" onPress={() => handleSendComment()} />
           </S.ButtonContent>
         </S.Section>
 
